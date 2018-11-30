14621fed82d33797209b1b89a5ed48855b64eeef
Simon Bertling <simon.bertling@gmx.de>
2018-11-30 21:07:03 +0900

    SCPIInterfaceTCP is now based on select

--- scpidev/interface.py
+++ scpidev/interface.py
@@ -3,6 +3,7 @@ import socket
 import time
 import threading
 import abc
+import select
 try:
     from queue import Queue
 except ImportError:
@@ -26,9 +27,40 @@ class SCPIInterfaceBase(object):
     implement the abstract methods."""
     def __init__(self):
         self._is_running = threading.Event()
+        self._recv_string_rest = ""
 
     def stop(self):
         self._is_running.clear()
+
+    def _parselines(self, recv_data):
+        """Returns a list of lines with line end characters. ``None`` if the 
+        recv_data is an empty string. An empty list is return if no newline 
+        character was received.
+        
+        Todo: This function is quite a plague to implement. Need to find out 
+        better methods...
+        """
+        if not recv_data:
+            # Remote host closed the connection when an empty string is 
+            # received. In that case, the rest string buffer is cleared.
+            self._recv_string_rest = ""
+            return None
+        recv_string_list = list([""])
+        recv_string = self._recv_string_rest + recv_data.decode("utf8")
+        if "\n" in recv_string:
+            # TODO: 
+            # - splitlines is also splitting lines for \r and others. 
+            # Usuall only \n is should be used as command delimiter.
+            recv_string_list = recv_string.splitlines(True)
+            last_string = recv_string_list.pop()
+            if "\n" in last_string:
+                self._recv_string_rest = ""
+                recv_string_list.append(last_string)
+            else:
+                self._recv_string_rest = last_string
+        else:
+            self._recv_string_rest = recv_string
+        return recv_string_list
 
     @abc.abstractmethod
     def write(self, data):
@@ -40,6 +72,9 @@ class SCPIInterfaceBase(object):
 
 
 class SCPIInterfaceTCP(SCPIInterfaceBase):
+    SELECT_TIMEOUT = 1
+    BUFFER_SIZE = 1024
+
     def __init__(self, *args, **kwargs):
         """Instantiates a TCP interface and binds to the socket. Exceptions 
         must be handled by the instance holder."""
@@ -56,137 +91,90 @@ class SCPIInterfaceTCP(SCPIInterfaceBase):
             port = 5025
 
         # Initialize member variables.
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._addr = (local_host, port)
         self._socket_remote = None
         self._remote_addr = None
         self._recv_string_rest = ""
 
         # Bind to TCP socket. Exceptions must be handled by instance holder.
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setblocking(0)
         self._socket.bind(self._addr)
-        self._socket.listen(1)
         logging.info("TCP socket bound to {}. Waiting for client connection"
             .format(self._addr))
 
     def __str__(self):
         return "TCP Interface {}".format(self._addr)
 
-    def _open(self):
-        self._socket_remote = None
-        self._remote_addr = None
-
-        # To make the waiting for connections non-blocking, the timeout is set
-        # to a reasonable low value.
-        timeout = self._socket.gettimeout()
-        self._socket.settimeout(1)
-        while self._is_running.is_set():
-            try:
-                self._socket_remote, self._remote_addr = self._socket.accept()
-                break
-            except socket.timeout:
-                continue
-        self._socket.settimeout(timeout)
-        if self._is_running.is_set():
-            logging.info("TCP client connection established: {}"
-                .format(self._remote_addr))
-
-    def _close(self):
-        self._close_remote()
-        self._socket.close()
-
-    def _close_remote(self):
-        if self._socket_remote:
-            logging.debug("Closing remote socket {}".format(self._remote_addr))
-            try:
-                self._socket_remote.shutdown(socket.SHUT_RDWR)
-                self._socket_remote.close()
-            except Exception as e:
-                logging.debug("Could not close remote socket. {}".format(e))
-        self._socket_remote = None
-        self._remote_addr = None
-
     def write(self, data):
-        bytes_written = self._socket_remote.send(data.encode("utf8"))
+        bytes_written = None
+        if self._socket_remote:
+            bytes_written = self._socket_remote.send(data.encode("utf8"))
         return bytes_written
-
-    def _read_data(self, buffer_size=1024):
-        # To make the waiting for connections non-blocking, the timeout is set
-        # to a reasonable low value.
-        timeout = self._socket.gettimeout()
-        self._socket_remote.settimeout(1)
-        data = b""
-        while self._is_running.is_set():
-            try:
-                data = self._socket_remote.recv(buffer_size)
-                break
-            except socket.timeout:
-                continue
-        self._socket.settimeout(timeout)
-        return data
-
-    def _readlines(self):
-        """Returns a list of lines with line end characters. This function is 
-        necessary, becase when using socket.makefile, the readline is blocking 
-        the thread.
-        
-        Todo: This function is quite a plague to implement. Need to find out 
-        better methods...
-        """
-        # Here we can use an infinite loop, because ``_read_data()`` is 
-        # non-blocking. When our client disconnects or the server is shut-down 
-        # an empty byte string is received.
-        while True:
-            recv_data = self._read_data()
-            if not recv_data:
-                # Remote host closed the connection when an empty string is 
-                # received. In that case, the rest string buffer is cleared.
-                self._recv_string_rest = ""
-                return list([""])
-            logging.debug("TCP received data: {!r}".format(recv_data))
-            recv_string = self._recv_string_rest + recv_data.decode("utf8")
-            logging.debug("TCP receive buffer: {!r}".format(recv_string))
-            if "\n" in recv_string:
-                # TODO: 
-                # - splitlines is also splitting lines for \r and others. 
-                # Usuall only \n is should be used as command delimiter.
-                recv_string_list = recv_string.splitlines(True)
-                last_string = recv_string_list.pop()
-                if "\n" in last_string:
-                    self._recv_string_rest = ""
-                    recv_string_list.append(last_string)
-                else:
-                    self._recv_string_rest = last_string
-                return recv_string_list
-            else:
-                self._recv_string_rest = recv_string
 
     def data_handler(self, recv_queue):
         """The ``data_handler()`` function will handle the connections to the 
         clients, receive data and fill the ``recv_queue`` with received 
-        commands. It will run until ``stop()`` is called."""
+        commands. It will run until ``stop()`` is called.
+        
+        TODO: Currently, I commented the exception handler out. This is 
+        because I want some errors during development to pop up. For 
+        production code, the data_handler should be self-sustaining.
+        """
+        inputs = [self._socket]
+        self._socket.listen(1)
+        
         self._is_running.set()
-        while self._is_running.is_set():
-            # TODO: Currently, I commented the exception handler out. This is 
-            # because I want to see some errors during development pop up.
-            # For production code, the data_handler should be self-sustaining.
-            # try:
-            self._open()
-            while self._is_running.is_set():
-                recv_string_list = self._readlines()
-                if not recv_string_list[0]:
-                    if self._is_running.is_set():
+        while self._is_running.is_set() and inputs:
+            # A timeout must be set to be able to catch the stop() event.
+            readables, writeables, exceptionals = select.select(
+                inputs, [], inputs, SCPIInterfaceTCP.SELECT_TIMEOUT)
+
+            for readable in readables:
+                if readable is self._socket:
+                    # The server is able to accept connections.
+                    if self._socket_remote is None:
+                        self._socket_remote, self._remote_addr = \
+                            readable.accept()
+                        self._socket_remote.setblocking(0)
+                        inputs.append(self._socket_remote)
+                        logging.info("TCP client connection established: {}"
+                            .format(self._remote_addr))
+                else:
+                    # The client has sent data.
+                    recv_data = readable.recv(SCPIInterfaceTCP.BUFFER_SIZE)
+                    logging.debug("TCP received data: {!r}".format(recv_data))
+                    recv_data_list = self._parselines(recv_data)
+                    if recv_data_list is None:
+                        # Received empty string => Connection closed by 
+                        # client.
+                        inputs.remove(readable)
+                        readable.close()
+                        if readable is self._socket_remote:
+                            self._socket_remote = None
+                            self._remote_addr = None
                         logging.info("TCP connection closed by client.")
-                    break
-                for recv_string in recv_string_list:
-                    if recv_string:
-                        data = (self, recv_string)
-                        recv_queue.put(data)
-            # except Exception as e:
-            #     logging.warning(
-            #         "Could not Receive data on interface {}. Exception: {}"
-            #         .format(self, e))
-            #     self._close_remote()
-        self._close()
+                    else:
+                        # Received ordinary data. Parse lines and put into the 
+                        # receive queue.
+                        for recv_string in recv_data_list:
+                            if recv_string:
+                                data = (self, recv_string)
+                                recv_queue.put(data)
+
+            for exceptional in exceptionals:
+                logging.warning("TCP Handler: Got one exceptional: {}"
+                    .format(exceptional))
+                self._inputs.remove(exceptional)
+                exceptional.close()
+        
+        # Close all open sockets.
+        for s in inputs:
+            try:
+                s.shutdown(socket.SHUT_RDWR)
+            except:
+                pass
+            s.close()
         logging.info("TCP handler has stopped. {}".format(self._addr))
 
 
